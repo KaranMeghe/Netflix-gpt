3a37f9f3e7c9011ba5be576e857c8e1cc2f369ba
Karan Meghe <karanmeghe87@gmail.com>
2025-01-30 02:00:56 +0530

    feat: add constants.js for centralized URL management
    - Created constants.js to store reusable URLs
    - Added LOGO and USER_ICON constants for Netflix logo and user icon URLs

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,6 +5,7 @@ import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { addUser, removeUser } from '../../Redux/store';
 import { auth } from '../../Utils/firebase';
 import { useEffect } from 'react';
+import { LOGO, USER_ICON } from '../../Utils/constatnt';
 
 
 const Header = () => {
@@ -49,10 +50,10 @@ const Header = () => {
 
     return (
         <div className='absolute px-8 py-2 bg-gradient-to-b from-black w-full h-full flex justify-between'>
-            <img src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="brand-logo" className='w-32 h-16' />
+            <img src={LOGO} alt="brand-logo" className='w-32 h-16' />
 
             {!user ? null : <div className='px-8 py-2 flex gap-2 items-center h-16'>
-                <img src='https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229' alt="usericon" className='w-8' />
+                <img src={USER_ICON} alt="usericon" className='w-8' />
                 <Button onClick={handleSubmit} className="text-white">Sign out</Button>
             </div>}
         </div>
